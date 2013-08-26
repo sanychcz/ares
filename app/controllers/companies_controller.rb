8f75d6548027d65adbfa6a8162362870e8b61eb5
sanychcz <alexander@420on.cz>
2013-08-26 18:14:02 +0200

    initial

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,21 @@
+class CompaniesController < ApplicationController
+  
+  def index
+  	require 'nokogiri'
+  
+  	#resp = open("http://wwwinfo.mfcr.cz/cgi-bin/ares/darv_bas.cgi?#{params[:ico]}")
+
+  	#@xml_doc = Nokogiri::XML(resp.read)
+  	@xml_doc = Nokogiri::XML(open("http://wwwinfo.mfcr.cz/cgi-bin/ares/darv_bas.cgi?ico=#{params[:ico]}&xml=0&ver=1.0.2"))
+  	@status_main = @xml_doc.xpath("//dtt:Nadpis").text
+  	@company_status = @xml_doc.xpath("//dtt:Stav_subjektu").text
+  	@company_ico = @xml_doc.xpath("//dtt:ICO").text
+  	@company_datum = @xml_doc.xpath("//dtt:Datum_zapisu_OR").text
+  	@company_name = @xml_doc.xpath("//dtt:Obchodni_firma").text
+  	@company_type = @xml_doc.xpath("//dtt:Nazev_PF").text
+  	@company_business = @xml_doc.xpath("//dtt:Text")
+  end
+
+  def new
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rejstrik::Application.config.secret_token = '8244f97ca0a918c5608b5058e2b276553dab3e2aad33ffbe0179ff4463dd7c7314170b6f104df0c9f18527f70a307ef9865e1ec2e90d165b19b2c10c0afa67b7'
