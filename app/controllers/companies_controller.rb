ba7a19aa4e7d4ca1978d63be294549c9d5d91d4a
sanychcz <alexander@420on.cz>
2013-08-27 14:35:52 +0200

    format tabs

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,22 +1,22 @@
 class CompaniesController < ApplicationController
   
   def index
-  	require 'nokogiri'
+    require 'nokogiri'
   
   	#resp = open("http://wwwinfo.mfcr.cz/cgi-bin/ares/darv_bas.cgi?#{params[:ico]}")
 
   	#@xml_doc = Nokogiri::XML(resp.read)
-  	@xml_doc = Nokogiri::XML(open("http://wwwinfo.mfcr.cz/cgi-bin/ares/darv_bas.cgi?ico=#{params[:company_search][:ico]}&xml=0&ver=1.0.2"))
-  	@status_main = @xml_doc.xpath("//dtt:Nadpis").text
-  	@company_status = @xml_doc.xpath("//dtt:Stav_subjektu").text
-  	@company_ico = @xml_doc.xpath("//dtt:ICO").text
-  	@company_datum = @xml_doc.xpath("//dtt:Datum_zapisu_OR").text
-  	@company_name = @xml_doc.xpath("//dtt:Obchodni_firma").text
-  	@company_type = @xml_doc.xpath("//dtt:Nazev_PF").text
-  	@company_business = @xml_doc.xpath("//dtt:Text")
+    @xml_doc = Nokogiri::XML(open("http://wwwinfo.mfcr.cz/cgi-bin/ares/darv_bas.cgi?ico=#{params[:company_search][:ico]}&xml=0&ver=1.0.2"))
+    @status_main = @xml_doc.xpath("//dtt:Nadpis").text
+    @company_status = @xml_doc.xpath("//dtt:Stav_subjektu").text
+    @company_ico = @xml_doc.xpath("//dtt:ICO").text
+    @company_datum = @xml_doc.xpath("//dtt:Datum_zapisu_OR").text
+    @company_name = @xml_doc.xpath("//dtt:Obchodni_firma").text
+    @company_type = @xml_doc.xpath("//dtt:Nazev_PF").text
+    @company_business = @xml_doc.xpath("//dtt:Text")
   end
 
   def create
-  	#@company_search = CompanySearch.new(params[:company_search])
+    #@company_search = CompanySearch.new(params[:company_search])
   end
 end
